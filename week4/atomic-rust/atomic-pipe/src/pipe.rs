a353b6d559fff5df0b01b82182ace28722540364
rishabhBudhouliya <rb5727@nyu.edu>
2026-08-04 18:02:38 -0400

    Added message struct
    - Purpose: This struct can be used to construct frames of two types: continuation and terminal
    - Objectives: message should allow storing necessary ingedients to construct a frame and
    convert the message into a owned buffer by the caller
    - Lifetime and scope: This message intends to allocate memory for the lifetime of a given payload
    The 'a lifetime tag denotes the idea that a message is tied to the payload.

--- week4/atomic-rust/atomic-pipe/src/pipe.rs
+++ week4/atomic-rust/atomic-pipe/src/pipe.rs
@@ -5,26 +5,43 @@
  * 3) add a test file that tests the pipe code
  */
 
-struct Message {
+struct Message<'a> {
     // process id, u32 should fit 2^22 ids
     id: u32,
     last: u8,
-    payload: [u8; 4091],
+    payload: &'a [u8],
     limit: usize,
 }
 
-impl Message {
-    pub fn new(id: u32, last: u8, payload: [u8; 4091]) -> Message {
-        Message { id, last, payload, 4096}
+// returned Message is tired to this payload
+impl<'a> Message<'a> {
+    pub fn new(id: u32, last: u8, payload: &'a [u8]) -> Message<'a> {
+        let limit = 4096;
+        Message {
+            id,
+            last,
+            payload,
+            limit,
+        }
     }
 
     // think of the frame buffer as a Vec<u8> instead of a fixed sized array
     pub fn convert(&self) -> Vec<u8> {
+        let word = (self.id | ((self.last as u32 & 1) << 22)).to_be_bytes();
         if self.last & 1 == 1 {
-            let word = self.id | ((self.last as u32 & 1) << 22);
             let size = (self.payload.len() as u16).to_be_bytes();
-            return (word << 8) + size + self.payload.to_be_bytes();
+            let mut buffer = Vec::with_capacity(self.payload.len() + 5);
+            // we need to slice the first byte to keep the 3 bytes of header (u32 is 4 bytes)
+            buffer.extend(&word[1..]);
+            buffer.extend(size);
+            buffer.extend(self.payload);
+            return buffer;
         } else {
+            let mut buffer = Vec::with_capacity(4096);
+            // we need to slice the first byte to keep 3 bytes
+            buffer.extend(&word[1..]);
+            buffer.extend(self.payload);
+            return buffer;
         }
     }
 }
